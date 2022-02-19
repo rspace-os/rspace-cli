ec07c296c89c6369b0dfd2b02475943f8eb1ac5d
richarda23 <richard@researchspace.com>
2022-02-19 10:37:02 +0000

    highlight help command

--- cmd/root.go
+++ cmd/root.go
@@ -40,7 +40,9 @@ Alternatively set these as environment variables.
 
 Environment variables override settings in the config file.
 
-To see all the ELN commands run rspace eln --help
+To see all the ELN commands run 
+
+    rspace eln --help
 `,
 	//	Run: func(cmd *cobra.Command, args []string) { },
 }
